a3bdfb8a348cda7f332b41201bf430d9a35d27cd
AwePhD <mathias.reus@gipsa-lab.fr>
2023-06-14 13:25:47 +0000

    fix: remove useless lines and add checkpoint loading

--- src/pstr.py
+++ src/pstr.py
@@ -4,10 +4,9 @@ from typing import Dict
 import torch
 from torch.utils.data import DataLoader
 import mmcv
-from mmdet.models import build_detector
-from mmcv.runner import wrap_fp16_model
-from mmdet.models import build_detector
+from mmcv.runner import load_checkpoint
 from mmcv.parallel import MMDataParallel
+from mmdet.models import build_detector
 from mmdet.datasets import (build_dataloader, build_dataset, replace_ImageToTensor)
 
 CONFIG_FILE = str(Path.cwd() / "configs" / "pstr" / "tdbaseline.py")
@@ -21,33 +20,9 @@ class PSTR:
     - It can run infer that outputs PSTR result for the whole dataset.
     """
     def _load_config(self, config_file: Path) -> mmcv.Config:
-        # Load configs file with boiler plate
-        cfg = mmcv.Config.fromfile(str(config_file))
+        cfg = mmcv.Config.fromfile(Path("./configs/pstr/pstr_r50_24e_cuhk.py"))
         cfg.model.pretrained = None
-        if cfg.model.get('neck'):
-            if isinstance(cfg.model.neck, list):
-                for neck_cfg in cfg.model.neck:
-                    if neck_cfg.get('rfp_backbone'):
-                        if neck_cfg.rfp_backbone.get('pretrained'):
-                            neck_cfg.rfp_backbone.pretrained = None
-            elif cfg.model.neck.get('rfp_backbone'):
-                if cfg.model.neck.rfp_backbone.get('pretrained'):
-                    cfg.model.neck.rfp_backbone.pretrained = None
-        if isinstance(cfg.data.test, dict):
-            cfg.data.test.test_mode = True
-            samples_per_gpu = cfg.data.test.pop('samples_per_gpu', 1)
-            if samples_per_gpu > 1:
-                # Replace 'ImageToTensor' to 'DefaultFormatBundle'
-                cfg.data.test.pipeline = replace_ImageToTensor(
-                    cfg.data.test.pipeline)
-        elif isinstance(cfg.data.test, list):
-            for ds_cfg in cfg.data.test:
-                ds_cfg.test_mode = True
-            samples_per_gpu = max(
-                [ds_cfg.pop('samples_per_gpu', 1) for ds_cfg in cfg.data.test])
-            if samples_per_gpu > 1:
-                for ds_cfg in cfg.data.test:
-                    ds_cfg.pipeline = replace_ImageToTensor(ds_cfg.pipeline)
+        cfg.data.test.test_mode = True
         cfg.model.train_cfg = None
 
         return cfg
@@ -66,9 +41,12 @@ class PSTR:
     def _load_model(self, weight_file: Path) -> MMDataParallel:
         # Get model and weights with boilerplate
         model = build_detector(self.config.model, test_cfg=self.config.get('test_cfg')).eval()
-        fp16_cfg = self.config.get('fp16', None)
-        if fp16_cfg is not None:
-            wrap_fp16_model(model)
+        load_checkpoint(
+            model,
+            str(Path.home() / "models" / "pstr_resnet_cuhk" / "pstr_r50_cuhk.pth"),
+            map_location='cpu'
+        )
+
         model.CLASSES = self.dataloader.dataset.CLASSES
 
         return MMDataParallel(model, device_ids=[0])
