eea09ea5699808ac75c5b1ab5139668779ff6c6d
AwePhD <mathias.reus@gipsa-lab.fr>
2023-06-15 15:42:20 +0000

    feat: Generation of captions and frame features
    Finalize the generation of the models output for captions and frames
    - For captions, we use a finetuned version of CLIP on PEDES to compute text features.
    - For images, we first detect them with a PSTR and export those detections in to a H5 file (see detection_generation.py). Then we can extract images features for each bboxes. It gives for a single frames 100 scores, bboxes and features
    - We organize the code to be partitioned in a meaninful way
    - Use of H5 file for outputs the result that would be reused for the evaluation script.

--- src/features_generation.py
+++ src/features_generation.py
@@ -8,6 +8,7 @@ from torch.utils.data import DataLoader
 from PIL import Image
 import torchvision.transforms as T
 from irra.model.clip_model import CLIP
+from tqdm import tqdm
 
 from tokenizer import SimpleTokenizer, tokenize
 from clip import load_clip, IMAGE_SIZE
@@ -20,18 +21,18 @@ H5_FILE = Path.cwd() / "outputs" / H5_FILENAME
 TOKEN_BATCH_SIZE = 512
 MEAN = [0.48145466, 0.4578275, 0.40821073]
 STD = [0.26862954, 0.26130258, 0.27577711]
-H5_FRAME_OUTPUT_FILENAME = "filename_to_frame_output"
-H5_CAPTIONS_OUTPUT_FILENAME = "filename_to_captions_output"
+H5_FRAME_OUTPUT_FILENAME = "filename_to_frame_output.h5"
+H5_CAPTIONS_OUTPUT_FILENAME = "filename_to_captions_output.h5"
 H5_FRAME_OUTPUT_FILE = Path.cwd() / "outputs" / H5_FRAME_OUTPUT_FILENAME
 H5_CAPTIONS_OUTPUT_FILE = Path.cwd() / "outputs" / H5_CAPTIONS_OUTPUT_FILENAME
 
 class FrameOutput(NamedTuple):
-    # (100,)
+    # (100, )
     scores: torch.Tensor
     # (100, 4)
     bboxes: torch.Tensor
     # (100, 512)
-    features: torch.Tensor
+    image_features: torch.Tensor
 
 class CaptionsOutput(NamedTuple):
     caption_1: torch.Tensor
@@ -132,7 +133,7 @@ def _compute_features_from_one_frame(
     crops = [
         frame.crop((
             int(bbox[0]),
-            int( bbox[1]),
+            int(bbox[1]),
             int(bbox[2]-bbox[0]),
             int(bbox[3]-bbox[1])
         ))
@@ -142,66 +143,62 @@ def _compute_features_from_one_frame(
     crops_preprocessed = torch.stack([preprocess_crop(crop) for crop in crops])
 
     with torch.no_grad():
-        crops_features = model.encode_image(crops_preprocessed.cuda()).cpu()
+        crops_features = (
+            model.encode_image(crops_preprocessed.cuda())
+            # Last layer + send to CPU
+            [:, 0, :].cpu()
+        )
 
     return crops_features
 
-def _get_image_features(
+def _compute_and_frame_output(
     model: CLIP,
     frame_file_to_detection: Dict[Path, DetectionOutput],
-) -> Dict[Path, torch.Tensor]:
-    # Take one frame at time and output the images features
+) -> Dict[Path, FrameOutput]:
     return {
-        frame_file: _compute_features_from_one_frame(model, frame_file, detection.bboxes)
-        for frame_file, detection in frame_file_to_detection.items()
+        frame_file: FrameOutput(
+            detection.scores,
+            detection.bboxes,
+            _compute_features_from_one_frame(model, frame_file, detection.bboxes),
+        )
+        for frame_file, detection in tqdm(frame_file_to_detection.items())
     }
-
-def export_frame_output_to_hdf5(
-    frame_file_to_frame_output: Dict[Path, FrameOutput],
-    h5_file: Path
-) -> None:
-    with h5py.File(h5_file, 'w') as f:
-        for frame_file, detection_output in frame_file_to_frame_output.items():
-            group = f.create_group(frame_file.name)
-
-            group.create_dataset('scores', data=detection_output.scores)
-            group.create_dataset('bboxes', data=detection_output.bboxes)
-            group.create_dataset('features', data=detection_output.features)
 
 def export_caption_features_to_hdf5(
     crop_index_to_captions_output: Dict[CropIndex, CaptionsOutput],
     h5_file: Path
 ) -> None:
+    if h5_file.exists():
+        h5_file.unlink()
+
     with h5py.File(h5_file, 'w') as f:
         for crop_index, captions_output in crop_index_to_captions_output.items():
-            group = f.create_group(crop_index)
+            group = f.create_group(f"p{crop_index.person_id}_s{crop_index.frame_id}")
 
             group.create_dataset('caption_1', data=captions_output.caption_1)
             group.create_dataset('caption_2', data=captions_output.caption_2)
 
 
+def export_frame_output_to_hdf5(
+    frame_file_to_frame_output: Dict[Path, FrameOutput],
+    h5_file: Path
+):
+    if h5_file.exists():
+        h5_file.unlink()
 
-def main():
-    model = load_clip()
-    model.cuda()
+    with h5py.File(h5_file, 'w') as f:
+        for frame_file, frame_output in frame_file_to_frame_output.items():
 
-    annotations = _import_annotations()
+            group = f.create_group(frame_file.name)
 
-    crop_index_to_captions_outputs = _get_text_features(annotations, model)
+            group.create_dataset('scores', data=frame_output.scores)
+            group.create_dataset('bboxes', data=frame_output.bboxes)
+            group.create_dataset('features', data=frame_output.features)
 
-    frame_file_to_detection= import_from_hdf5(H5_FILE, FRAME_FOLDER)
-    frame_to_image_features = _get_image_features(model, frame_file_to_detection)
-    frame_file_to_full_output = {
-        frame_file: FrameOutput(
-            frame_file_to_detection[frame_file].scores,
-            frame_file_to_detection[frame_file].bboxes,
-            frame_to_image_features[frame_file]
-        )
-        for frame_file in frame_file_to_detection
-    }
-
-    # Assure that annotations (used for evaluation) and the output
-    # of the model (used for detection compute) are the same
+def _assert_detection_output_and_annotations_compatibility(
+    annotations: pd.DataFrame,
+    frame_file_to_detection: Dict[Path, DetectionOutput]
+) -> None:
     annotations_frame_ids = set(
         f"s{frame_id}.jpg"
         for frame_id in annotations.index.get_level_values("frame_id").unique()
@@ -212,9 +209,38 @@ def main():
     )
     assert annotations_frame_ids == h5_frame_ids
 
-    # Export them
-    export_frame_output_to_hdf5(frame_file_to_full_output, H5_FRAME_OUTPUT_FILE)
-    export_caption_features_to_hdf5(crop_index_to_captions_outputs, H5_CAPTIONS_OUTPUT_FILE)
+
+def _generate_captions_output(
+    annotations: pd.DataFrame,
+    model: CLIP,
+    h5_file: Path = H5_CAPTIONS_OUTPUT_FILE,
+) -> None:
+    crop_index_to_captions_outputs = _get_text_features(annotations, model)
+    export_caption_features_to_hdf5(crop_index_to_captions_outputs, h5_file)
+
+
+def _generate_frame_output(
+    frame_file_to_detection: Dict[Path, DetectionOutput],
+    model: CLIP,
+    h5_file: Path = H5_FRAME_OUTPUT_FILE,
+) -> None:
+    frame_file_to_frame_output = _compute_and_frame_output(model, frame_file_to_detection)
+    export_frame_output_to_hdf5(frame_file_to_frame_output, h5_file)
+
+
+def main():
+    model = load_clip().eval().cuda()
+
+    annotations = _import_annotations()
+
+    _generate_captions_output(annotations, model)
+
+    frame_file_to_detection= import_from_hdf5(H5_FILE, FRAME_FOLDER)
+    # Assure that annotations (used for evaluation) and the output
+    # of the model (used for detection compute) have the same FRAMES
+    _assert_detection_output_and_annotations_compatibility(annotations, frame_file_to_detection)
+
+    _generate_frame_output(frame_file_to_detection, model)
 
 if __name__ == "__main__":
     main()
