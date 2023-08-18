8697ea5c3170b49ea810417749d3c853dc5d09d0
AwePhD <mathias.reus@gipsa-lab.fr>
2023-08-18 07:51:29 +0000

    feat: add arg option for origin of the crop features, rename with consistent logic with other scripts

---
+++ scripts/evaluate_text_reid.py
@@ -0,0 +1,31 @@
+import argparse
+from pathlib import Path
+
+from tdbaseline.text_reid.eval import evaluate_from_ground_truth
+from tdbaseline.config import get_config
+
+def _get_crop_origin_from_args() -> str:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--type", type=str, choices=["files", "annotations"], required=True)
+
+    return parser.parse_args().type
+
+
+def main():
+    config = get_config(Path('./config.yaml'))
+
+    crop_origin = _get_crop_origin_from_args()
+    crop_features_file = Path(
+        config['h5_files'][f'crop_features_from_{crop_origin}']
+    )
+
+    mean_average_precision = evaluate_from_ground_truth(
+        crop_features_file,
+        Path(config['h5_files']['captions_output'])
+    )
+    print(f"mAP: {mean_average_precision:.2%}")
+
+
+if __name__ == "__main__":
+    main()
+
